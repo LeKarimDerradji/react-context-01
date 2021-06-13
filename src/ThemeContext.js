aa26ddcaeb322389d0c9c6abf664103f7aed2de1
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-06-13 16:55:26 +0200

    createContext(null)

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 // Creating a context object called ThemeContext, set the inital value to Null.
-const ThemeContext = React.createContext("color")
+const ThemeContext = React.createContext(null)
 
 console.log(ThemeContext);
 
